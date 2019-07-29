befa5a882a2881d627e687d615cfea694bfb84a3
neired <nmillans91@gmail.com>
2019-07-29 09:12:38 +0200

    Add functions for random number creation and input showing in console

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,21 +1,34 @@
 'use strict';
 
-// 2. generar un número aleatorio con la ayuda de Math.random y Math.ceil, y mostrarlo por la consola
-// (podéis usar la función de este codepen)
+//Generar un número aleatorio hasta 100 y mostrarlo en la consola
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
 
-// 3. al pulsar el botón de "prueba", acceder al contenido del input y mostrarlo en la consola
-//constantes
+const myRandomNumber = getRandomNumber(100);
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+// Al pulsar el botón de "prueba", acceder al contenido del input y mostrarlo en la consola
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
-//handler
-function showConsole(event){
-  console.log(input.innerHTML);
-}
-//listener
-btn.addEventListener('keyup',showConsole);
 
+function showConsole(event){
+  console.log(input.value);
+}
+
+btn.addEventListener('click',showConsole);
 
 // 4. comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback
 // correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS GANADO, CAMPEONA!")
+const clue = do
+function compareNumbers {
+  if (input.value === myRandomNumber){
+    "¡HAS GANADO, CAMPEONA!"
+  } else if {
+    "demasiado alto"
+  } else {
+    "demasiado bajo"
+  }
+}
 // 5. actualizar el contador de intentos cada vez que el usuario pruebe
 
